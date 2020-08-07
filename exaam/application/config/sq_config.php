275d16f207a805bcc62ec9ffaf6f1a534d072c5f
SE-MohamedSalem <se.mohamedsalem@gmail.com>
2020-08-07 22:29:55 +0200

    FinalProject

---
+++ exaam/application/config/sq_config.php
@@ -0,0 +1,7 @@
+<?php 
+		$sq_base_url='http://[::1]/exaam/';
+		$sq_hostname='localhost';
+		$sq_dbname='savsoftquiz_v5.0';
+		$sq_dbusername='root';
+		$sq_dbpassword='';
+		?>
---
+++ exaam/application/logs/log-2020-08-07.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-08-07 09:44:58 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp2\htdocs\exaam\application\config\config.php 515
+ERROR - 2020-08-07 09:55:59 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp2\htdocs\exaam\application\config\config.php 515
+ERROR - 2020-08-07 09:56:00 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp2\htdocs\exaam\application\config\config.php 515
+ERROR - 2020-08-07 09:56:01 --> Severity: Warning --> mysqli::__construct(): (HY000/1045): Access denied for user ''@'localhost' (using password: NO) C:\xampp2\htdocs\exaam\application\config\config.php 515
+ERROR - 2020-08-07 10:05:55 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\config\config.php 524
+ERROR - 2020-08-07 10:05:55 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\config\config.php 524
+ERROR - 2020-08-07 10:06:31 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\config\config.php 524
+ERROR - 2020-08-07 10:06:47 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\config\config.php 524
+ERROR - 2020-08-07 10:07:14 --> Severity: Warning --> Division by zero C:\xampp2\htdocs\exaam\application\views\dashboard.php 237
+ERROR - 2020-08-07 10:08:30 --> Severity: Warning --> implode(): Invalid arguments passed C:\xampp2\htdocs\exaam\application\models\Quiz_model.php 155
+ERROR - 2020-08-07 15:39:33 --> Severity: Warning --> Division by zero C:\xampp2\htdocs\exaam\application\views\dashboard.php 237
+ERROR - 2020-08-07 15:43:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\models\User_model.php 468
+ERROR - 2020-08-07 15:43:56 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp2\htdocs\exaam\system\libraries\Email.php 1896
+ERROR - 2020-08-07 15:46:15 --> Severity: Warning --> Division by zero C:\xampp2\htdocs\exaam\application\views\dashboard.php 237
+ERROR - 2020-08-07 15:47:12 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\models\User_model.php 421
+ERROR - 2020-08-07 15:48:40 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\models\Quiz_model.php 878
+ERROR - 2020-08-07 15:49:23 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp2\htdocs\exaam\system\libraries\Email.php 1896
+ERROR - 2020-08-07 15:49:23 --> Could not find the language line "hello"
+ERROR - 2020-08-07 15:49:23 --> Could not find the language line "user_id"
+ERROR - 2020-08-07 15:49:23 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp2\htdocs\exaam\application\views\view_result.php 356
+ERROR - 2020-08-07 22:08:00 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\models\Quiz_model.php 878
+ERROR - 2020-08-07 22:08:02 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\models\Quiz_model.php 878
+ERROR - 2020-08-07 22:08:04 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp2\htdocs\exaam\application\models\Quiz_model.php 878
+ERROR - 2020-08-07 22:08:09 --> Severity: Warning --> mail(): Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() C:\xampp2\htdocs\exaam\system\libraries\Email.php 1896
+ERROR - 2020-08-07 22:08:09 --> Could not find the language line "hello"
+ERROR - 2020-08-07 22:08:09 --> Could not find the language line "user_id"
+ERROR - 2020-08-07 22:08:09 --> Severity: Warning --> count(): Parameter must be an array or an object that implements Countable C:\xampp2\htdocs\exaam\application\views\view_result.php 356
